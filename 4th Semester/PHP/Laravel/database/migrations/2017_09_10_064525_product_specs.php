293f1f61b9023a8254e6cbb2427891360aadb052
Bishal Thapa Shrestha <bishalthapashrestha@users.noreply.github.com>
2017-10-08 00:33:07 +0545

    Updated PHP project

---
+++ 4th Semester/PHP/Laravel/database/migrations/2017_09_10_064525_product_specs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductSpecs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('product_id');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
